4fd53314726ab1cb9c7578a5a84faabd5f8a7e79
martin_elrod <martin@squirro.com>
2017-10-27 11:35:14 +0200

    simpler solution for 48

--- 48.py
+++ 48.py
@@ -6,12 +6,5 @@ The series, 11 + 22 + 33 + ... + 1010 = 10405071317.
 Find the last ten digits of the series, 1**1 + 2**2 + 3**3 + ... + 1000** 1000.
 """
 
-
-euler48 = "dd"
-prod = 0
-
-for e in range(1,1001):
-    prod += e**e
-     
-
-print str(prod)[-10:]
+euler48 = str(sum(e**e for e in range(1,1001)))[-10:]
+print euler48
