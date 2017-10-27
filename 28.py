cc1233d3687a3bb16ac20b9810a6f4c9c9467534
martin_elrod <martin@squirro.com>
2017-10-27 11:30:26 +0200

    submit solution for 48.py

--- 28.py
+++ 28.py
@@ -16,5 +16,4 @@ Starting with the number 1 and moving to the right in a clockwise direction a 5 
 
     What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed in the same way?
 
-
 """
---
+++ 48.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python2
+
+"""
+The series, 11 + 22 + 33 + ... + 1010 = 10405071317.
+
+Find the last ten digits of the series, 1**1 + 2**2 + 3**3 + ... + 1000** 1000.
+"""
+
+
+euler48 = "dd"
+prod = 0
+
+for e in range(1,1001):
+    prod += e**e
+     
+
+print str(prod)[-10:]
