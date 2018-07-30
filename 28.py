94f212ed87b88bdd4b9971d98c09a3ff06d8c9d2
Your Name <you@example.com>
2018-07-30 12:09:26 +0200

    add solution 28

--- 28.py
+++ 28.py
@@ -17,3 +17,27 @@ Starting with the number 1 and moving to the right in a clockwise direction a 5 
     What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed in the same way?
 
 """
+
+def main():
+    solution = 0
+
+    max_height = 1001
+    #max_height = 5
+
+    diag = 1
+    last = 1
+
+    for height in range(3, max_height+1, 2):
+        for side in range(4):
+            last = last + height - 1
+            diag += last
+            #print("DEBUG: height: %d, last: %d" %(height, last))
+
+
+    solution = diag
+    print("Solution: %d" % solution)
+
+
+
+if __name__ == "__main__":
+    main()
