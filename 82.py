647a1155712a541319f2c859a57ccd50938682e2
zrthstr <zrth1k@gmail.com>
2020-06-25 23:24:59 +0200

    add lattice

--- 82.py
+++ 82.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env  python3
 
 """
-In the 5 by 5 matrix below, the minimal path sum from the top left to the bottom right, by only moving to the right and down, is indicated in bold red and is equal to 2427.
+Problem 82 - Path sum: three ways
+￼  ￼  ￼
+NOTE: This problem is a more challenging version of Problem 81.
 
-[[cant be copy pasted, see: https://projecteuler.net/problem=81]]
+The minimal path sum in the 5 by 5 matrix below, by starting in any cell in the left column and finishing in any cell in the right column, and only moving up, down, and right, is indicated in red and bold; the sum is equal to 994.
 
-Find the minimal path sum from the top left to the bottom right by only moving right and down in matrix.txt (right click and "Save Link/Target As..."), a 31K text file containing an 80 by 80 matrix.
+[[ see image: https://projecteuler.net/problem=82 ]]
 
+Find the minimal path sum from the left column to the right column in matrix.txt (right click and "Save Link/Target As..."), a 31K text file containing an 80 by 80 matrix.
 """
 
 import copy
